8181ac1246a87880b05668755a3406d21f13b856
JoaoIto <joaovictorpfr@gmail.com>
2026-02-02 13:00:03 -0300

    HOTFIX: I updated the build command in package.json to run npx prisma generate && next build

--- app/api/parties/route.ts
+++ app/api/parties/route.ts
@@ -2,6 +2,8 @@ import { auth } from '@/auth';
 import { prisma } from '@/app/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const session = await auth();
     if (!session?.user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
